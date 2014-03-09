139bb8d977722cd8d22461eb05b464349d338cb2
Lukas Rieger <lr-93@live.de>
2014-03-09 23:19:42 +0100

    ads

--- DuckTypingFS/Library1.fs
+++ DuckTypingFS/Library1.fs
@@ -7,6 +7,8 @@ open System.Reflection.Emit
 open System.Diagnostics.Contracts
 open System.Collections
 open System.Collections.Generic
+open System.Threading
+open System.Threading.Tasks
 
 type MethodSignature =
     | MethodSignature of returnType : Type * name : string * parameters : Type array
@@ -22,6 +24,7 @@ type MemberSignature =
     | Property of PropertySignature
     | Event of EventSignature
     | Other
+    
 
 let getMethodSignature (methodInfo : MethodInfo) =
         let parameters =  methodInfo.GetParameters() |> Array.map (fun p -> p.ParameterType)
@@ -73,34 +76,86 @@ let getAllMembers (tp:Type) =
 
 exception DuckExceptionMemberMapping of MemberSignature array
 
+/// the attributes of the generated type
+let GeneratedTypeAttributes = TypeAttributes.Public ||| TypeAttributes.Class ||| TypeAttributes.AutoClass |||
+                              TypeAttributes.AnsiClass ||| TypeAttributes.BeforeFieldInit ||| TypeAttributes.AutoLayout
+
+/// the attributes of a normal public .ctor
+let ConstructorAttributes = MethodAttributes.Public ||| MethodAttributes.SpecialName ||| MethodAttributes.RTSpecialName ||| MethodAttributes.HideBySig
+
+/// Attributes for a method implementing an interface
+let InterfaceImplementationAttributes = MethodAttributes.Public ||| MethodAttributes.SpecialName ||| MethodAttributes.NewSlot |||
+                                        MethodAttributes.HideBySig ||| MethodAttributes.Virtual ||| MethodAttributes.Final
+
+/// Attributes for a method/property overriding a virtual/abstract base
+let OverrideAttributes = MethodAttributes.Public ||| MethodAttributes.HideBySig ||| MethodAttributes.SpecialName ||| MethodAttributes.Virtual
+
+/// the Attributes for a normal, nonvirtual instance method
+let PrivateInstanceMethodAttributes = MethodAttributes.Private ||| MethodAttributes.HideBySig
+
+/// the Attributes for a normal, nonvirtual instance method
+let PrivateStaticMethodAttributes = MethodAttributes.Private ||| MethodAttributes.HideBySig ||| MethodAttributes.Static
+
+let mapType (duckType: Type) (thingType: Type) =
+    Contract.Requires duckType.IsInterface
+    //get all members of the duck and the thing
+    let duckMembers = getAllMembers duckType |> Seq.toList
+    let thingMembers = getAllMembers thingType |> Seq.toList
+    //try to map all duckmembers to thingmembers
+    let mapping = duckMembers
+                    |> Seq.map (fun x -> (x, findDuckMemberInThing x thingMembers))
+                    |> Seq.toList
+    //see if the mapping was successfull
+    let mappingSuccess = mapping |> Seq.forall (fun (d,t) -> t.IsSome)
+    if not mappingSuccess then
+        let missingMembers = mapping |> Seq.where (fun (d,t) -> t.IsNone)
+                                |> Seq.map (fun ((s, mi),t)->s) |> Seq.toArray
+        raise (DuckExceptionMemberMapping(missingMembers))
+    //we are still alive, so the mapping was a success
+    let assemblyName = sprintf "%s_%s_%s" duckType.Name thingType.Name (Guid.NewGuid().ToString())
+    let asm = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(assemblyName),
+                                                            Emit.AssemblyBuilderAccess.RunAndSave)
+    let dynMod = asm.DefineDynamicModule("MainModule", assemblyName + ".mod.dll", true)
+    let tb = dynMod.DefineType(sprintf "__%s_duck" duckType.Name, GeneratedTypeAttributes)
+    let ctor = tb.DefineConstructor(ConstructorAttributes, CallingConventions.HasThis, [| thingType |])
+    let ctorIL = ctor.GetILGenerator()
+
+
+
+    //emit the default constructor
+    ctorIL.Emit(OpCodes.Ldarg_0)
+    ctorIL.Emit(OpCodes.Call, typedefof<Object>.GetConstructor(Type.EmptyTypes))
+    ctorIL.Emit(OpCodes.Nop)
+    ctorIL.Emit(OpCodes.Ret)
+
+    let generatedType = tb.CreateType()
+    asm.Save(sprintf "%s.dll" assemblyName)
+    generatedType
+
+
 type DuckTyping =
   class
+    static member private _mappings = new Dictionary<Tuple<Type,Type>, Type>()
+    static member private _lock = new Object()
+
+    ///Casts a Thing to an interfae IDuck.
+    ///The thing must implement all members of IDuck.
     static member Cast<'TDuck when 'TDuck: null and 'TDuck : not struct> thing =
-        let duckType = typedefof<'TDuck>
-        let thingType = thing.GetType()
-        Contract.Requires duckType.IsInterface
         Contract.Requires (thing <> null)
-        //get all members of the duck and the thing
-        let duckMembers = getAllMembers duckType |> Seq.toList
-        let thingMembers = getAllMembers thingType |> Seq.toList
-        //try to map all duckmembers to thingmembers
-        let mapping = duckMembers
-                      |> Seq.map (fun x -> (x,findDuckMemberInThing x thingMembers))
-                      |> Seq.toList
-        //see if the mapping was successfull
-        let mappingSuccess = mapping |> Seq.forall (fun (d,t) -> t.IsSome)
-        if not mappingSuccess then
-            let missingMembers = mapping |> Seq.where (fun (d,t) -> t.IsNone)
-                                 |> Seq.map (fun ((s, mi),t)->s) |> Seq.toArray
-            raise (DuckExceptionMemberMapping(missingMembers))
-        //we are still alive, so the mapping was a success
-        let assemblyName = sprintf "%s_%s_%s" duckType.Name thingType.Name (Guid.NewGuid().ToString())
-        let asm = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(assemblyName),
-                                                                Emit.AssemblyBuilderAccess.RunAndSave)
-        let dynMod = asm.DefineDynamicModule("MainModule", assemblyName + ".mod.dll", true)
-        let tb = dynMod.DefineType(sprintf "__%s" duckType.Name)
-        let ctor = tb.DefineConstructor(MethodAttributes.)
-        let nullptr : 'TDuck = null
-        nullptr
+        let tDuck = typedefof<'TDuck>
+        let tThing = thing.GetType()
+        Monitor.Enter (DuckTyping._lock)
+        try
+            let key : Tuple<Type,Type> = new System.Tuple<Type,Type>(tDuck, tThing)
+            let mappedType =
+                if DuckTyping._mappings.ContainsKey (key) then
+                    DuckTyping._mappings.[key]
+                else
+                    let mt = mapType  tDuck tThing
+                    DuckTyping._mappings.Add(key, mt)
+                    mt
+            Activator.CreateInstance(mappedType, [| thing |]) :?> 'TDuck
+        finally
+            Monitor.Exit (DuckTyping._lock)
   end
 
