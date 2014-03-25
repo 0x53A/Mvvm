596bb42c26b7f899ef4df6d7395ac6ff7ff4c2e0
Lukas Rieger <lr-93@live.de>
2014-03-25 01:51:36 +0100

    ad

--- DuckTypingFS/Ducktyping.Internal.fs
+++ DuckTypingFS/Ducktyping.Internal.fs
@@ -1,5 +1,5 @@
 ﻿
-module DuckTypingFS
+module DuckTypingFSInternal
 
 open System
 open System.Reflection
@@ -237,38 +237,4 @@ let mapType (duckType: Type) (thingType: Type) =
     let realDelegates = delegates |> List.map (fun (a,b) -> (toField (findDuckMemberInThing (Field(a)) membersOfRealType).Value, b))
     (generatedType, realDelegates)
     
-type MappingKey = { TDuck : Type; TThing : Type }
-type MappingValue = { T : Type; Delegates : (FieldInfo*MethodInfo) list }
-
-type DuckTyping =
-  class
-    static member private _mappings = new Dictionary<MappingKey, MappingValue>()
-    static member private _lock = new Object()
-
-    //Casts a Thing to an interface IDuck.
-    //The thing must implement all members of IDuck.
-    static member Cast<'TDuck when 'TDuck: null and 'TDuck : not struct> thing =
-        Contract.Requires (thing <> null)
-        let tDuck = typedefof<'TDuck>
-        let tThing = thing.GetType()
-        
-
-        let key = { TDuck = tDuck; TThing = tThing }
-        let mv = lock (DuckTyping._lock) (fun x ->
-            if DuckTyping._mappings.ContainsKey (key) then
-                DuckTyping._mappings.[key]
-            else
-                let (t,del) = mapType tDuck tThing
-                let value = { T = t; Delegates = del }
-                DuckTyping._mappings.Add(key, value)
-                value  
-           )
-
-        let instance = Activator.CreateInstance(mv.T, [| thing |]) :?> 'TDuck
-              
-        for (a,b) in mv.Delegates do
-            let deleg = b.CreateDelegate(a.FieldType, thing)
-            a.SetValue(instance, deleg)
-        instance
-  end
 
---
+++ MvvmTests/Math3D.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mvvm.Math3D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace MvvmTests
+{
+    [TestClass]
+    public class Math3D
+    {
+        [TestMethod]
+        public void TestPlane()
+        {
+            var v3 = new Vector3(1, 2, 3);
+
+            var plane = new Plane(new Point3(0, 0, -10), new Vector3(0, 1, 0), new Vector3(1, 0, 0));
+            var mapped = plane.Map(new Point2(2, 5));
+            Assert.IsTrue(mapped.Z == -10);
+            Assert.IsTrue(mapped.X == 5);
+            Assert.IsTrue(mapped.Y == 2);
+        }
+    }
+}
