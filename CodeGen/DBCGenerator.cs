48b10205d0b6e8e5d6225be031fe7faedfbf4628
Lukas Rieger <lr@live.de>
2014-04-07 09:53:59 +0200

    Revert "Revert "asd""
    This reverts commit 47370da3877ac0ef78d9c7414d3d0809b9fe0535.

--- CodeGen/DBCGenerator.cs
+++ CodeGen/DBCGenerator.cs
@@ -148,8 +148,9 @@ namespace Mvvm.CodeGen
             else
                 mb = ab.DefineDynamicModule("generated");
 
-            var attr = targetType.GetCustomAttribute<GeneratedTypeNameAttribute>();
-            var name = attr != null ? attr.TypeName : targetType.FullName;
+            var attr = targetType.GetCustomAttribute<TypeOverrideAttribute>();
+            var name = attr != null ? attr.TypeName : targetType.Namespace;
+            name = name ?? targetType.FullName;
             var tb = mb.DefineType(name, CodeGenInternal.GeneratedTypeAttributes, null, new[] { targetType, typeof(INotifyPropertyChanged) });
 
             var constructor = tb.DefineConstructor(CodeGenInternal.ConstructorAttributes, CallingConventions.HasThis, null);
