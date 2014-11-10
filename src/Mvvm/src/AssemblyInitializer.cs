97af553860b4d6cf898e6df95d6f39115f3dd0da
lrieger <lr@live.de>
2014-11-11 00:35:15 +0100

    AssemblyInitializer

---
+++ src/Mvvm/src/AssemblyInitializer.cs
@@ -0,0 +1,57 @@
+﻿#if !UNIVERSAL
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+
+namespace Mvvm
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    sealed class AssemblyInitializerAttribute : Attribute
+    {
+        public AssemblyInitializerAttribute()
+        {
+        }
+    }
+
+    public interface IAssemblyInitializer
+    {
+        void Init();
+    }
+
+    public class  AssemblyInitializerMultipleTypesFoundException : Exception
+    {
+
+    }
+
+    public static class AssemblyInitializer
+    {
+        public static IAssemblyInitializer Find()
+        {
+            return Find(System.Reflection.Assembly.GetEntryAssembly());
+        }
+
+        public static IAssemblyInitializer Find(Assembly asm)
+        {
+            var suitableTypes = asm.GetExportedTypes()
+                .Where(t =>
+                    t.GetTypeInfo().GetInterfaces().Contains(typeof(IAssemblyInitializer)) &&
+                    t.GetTypeInfo().GetCustomAttribute<AssemblyInitializerAttribute>() != null)
+                .ToArray();
+            if (suitableTypes.Length == 0)
+                return null;
+            else if (suitableTypes.Length == 1)
+            {
+                var instance = (IAssemblyInitializer)Activator.CreateInstance(suitableTypes.Single());
+                return instance;
+            }
+            else
+                throw new AssemblyInitializerMultipleTypesFoundException();
+        }
+    }
+}
+
+#endif
