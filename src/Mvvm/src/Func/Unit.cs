cb8aacb35ebc5713977190d1392c95ff89496b5e
lrieger <lrieger@nemetschek-engineering.com>
2014-12-22 16:59:24 +0100

    Add unit type

---
+++ src/Mvvm/src/Func/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mvvm.Func
+{
+    public sealed class Unit
+    {
+        private readonly Unit _instance = new Unit();
+        public Unit Instance { get { return _instance; } }
+        private Unit() { }
+    }
+}
